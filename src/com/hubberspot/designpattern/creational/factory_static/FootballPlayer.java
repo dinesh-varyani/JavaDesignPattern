0cff07866c9925cb2cf46f98646c9332201e9166
Dinesh Varyani <java.hubberspot@gmail.com>
2018-09-13 21:11:46 +0530

    Initial commit

---
+++ src/com/hubberspot/designpattern/creational/factory_static/FootballPlayer.java
@@ -0,0 +1,8 @@
+package com.hubberspot.designpattern.creational.factory_static;
+
+public class FootballPlayer implements Player {
+    @Override
+    public void play() {
+        System.out.println("Football player playing ...");
+    }
+}
---
+++ src/com/hubberspot/designpattern/creational/factory_static/Player.java
@@ -0,0 +1,5 @@
+package com.hubberspot.designpattern.creational.factory_static;
+
+public interface Player {
+    public void play();
+}
